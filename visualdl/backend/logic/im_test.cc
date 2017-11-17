969fd9281d3f5922ca52fedb305fbe4a39ee43d8
superjom <superjom@gmail.com>
2017-11-17 16:49:40 +0800

    init information maintainer

---
+++ visualdl/backend/logic/im_test.cc
@@ -0,0 +1,30 @@
+#include "visualdl/backend/logic/im.h"
+
+#include "gtest/gtest.h"
+
+namespace visualdl {
+
+class ImTester : public ::testing::Test {
+protected:
+  void SetUp() override {}
+
+  InformationMaintainer &im = InformationMaintainer::Global();
+};
+
+TEST_F(ImTester, AddTablet) { im.AddTablet("tag0", 20); }
+
+TEST_F(ImTester, AddRecord) {
+  storage::Record rcd;
+  rcd.set_dtype(storage::DataType::kInt32s);
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 10; j++) {
+      rcd.mutable_data()->add_i32s(i * 20 + j);
+    }
+    im.AddRecord("tag0", storage::Tablet::Type::Tablet_Type_kGraph,
+                 rcd.SerializeAsString());
+  }
+
+  ASSERT_EQ(im.storage().Find("tag0")->records_size(), 20UL);
+}
+
+} // namespace visualdl
--- visualdl/backend/storage/storage.h
+++ visualdl/backend/storage/storage.h
@@ -9,24 +9,33 @@
 namespace visualdl {
 
 class Storage final {
- public:
-  /*
-   * There should be only one Storage instance in memory.
-   */
-  Storage &Global() {
-    static Storage *instance = new Storage();
-    return *instance;
+public:
+  Storage() {
+    // set time stamp
+    time_t time0;
+    time(&time0);
+    proto_.set_timestamp(time0);
   }
 
   /*
    * Add a new tablet named `tag`, the newly added instance will be returned.
    */
-  storage::Tablet *Add(const std::string &tag);
+  storage::Tablet *Add(const std::string &tag, int num_samples);
 
   /*
    * Search the tablet named `tag`, if not exist, return nullptr.
    */
-  const storage::Tablet *Find(const std::string &tag) const;
+  storage::Tablet *Find(const std::string &tag);
+
+  /*
+   * Append a new record to the tail of tablet.
+   */
+  storage::Record *NewRecord(const std::string &tag);
+
+  /*
+   * Get a record at `offset`, if the offset is not valid, yield a failed CHECK.
+   */
+  storage::Record *GetRecord(const std::string &tag, int offset);
 
   /*
    * Serialize this object to string and save it to a file.
@@ -38,7 +47,11 @@ class Storage final {
    */
   void Load(const std::string &path);
 
- protected:
+  storage::Storage *mutable_data() { return &proto_; }
+
+  const storage::Storage &data() { return proto_; }
+
+protected:
   /*
    * Serialize the Storage instance to string.
    */
@@ -49,17 +62,10 @@ class Storage final {
    */
   void DeSerialize(const std::string &data);
 
-  Storage() {
-    // set time stamp
-    time_t time0;
-    time(&time0);
-    proto_.set_timestamp(time0);
-  }
-
- private:
+private:
   storage::Storage proto_;
 };
 
 } // namespace visualdl
 
-#endif //VISUALDL_STORAGE_H
+#endif // VISUALDL_STORAGE_H
