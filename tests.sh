0ef22aa1c0d15faafea8a34bfab771c669dc3017
superjom <superjom@gmail.com>
2017-12-12 13:03:19 +0800

    update

---
+++ tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+sudo apt-get upgrade cmake -y
+mkdir -p build
+cd build
+cmake ..
+make
+make test
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then bash ./travis/run_on_pull_requests; fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then bash ./travis/run_on_non_pull_requests; fi
