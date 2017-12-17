27b65d4b2866b311ca8251ed1ff6752587abb3cf
Superjom <superjom@gmail.com>
2017-12-17 11:45:53 +0800

    code clean

--- visualdl/logic/im.cc
+++ visualdl/logic/im.cc
@@ -25,27 +25,6 @@ int ReserviorSample(int num_samples, int num_records) {
   return -1;
 }
 
-// IM::IM(StorageBase::Type type, StorageBase::Mode mode) {
-//   switch (type) {
-//     case StorageBase::Type::kMemory: {
-//       storage_.reset(new MemoryStorage);
-//     } break;
-//     default:
-//       CHECK(false) << "Unsupported storage kind " << type;
-//   }
-
-//   switch (mode) {
-//     case StorageBase::Mode::kRead:
-//       dynamic_cast<MemoryStorage *>(storage_.get())->StartReadService(500);
-//       break;
-//     case StorageBase::Mode::kWrite:
-//       dynamic_cast<MemoryStorage *>(storage_.get())->StartWriteSerice(500);
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
 void IM::SetPersistDest(const std::string &path) {
   CHECK(storage_->mutable_data()->dir().empty())
       << "duplicate set storage's path";
