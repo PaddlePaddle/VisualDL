64885033355471be1c7625b27fcd100f4642d203
superjom <superjom@gmail.com>
2017-12-22 17:31:56 +0800

    add scalar

--- visualdl/logic/sdk.h
+++ visualdl/logic/sdk.h
@@ -11,15 +11,30 @@ namespace components {
  * Read and write support for Scalar component.
  */
 template <typename T>
-class Scalar {
-public:
-  Scalar(Tablet tablet) : tablet_(tablet) { tablet_->SetTag(kScalar); }
+struct Scalar {
+  Scalar(Tablet tablet) : tablet_(tablet) { tablet_.SetType(Tablet::Type::kScalar);}
   void SetCaption(const std::string cap) {
-    tablet_->SetCaptions(std::vector<std::string>({cap}));
+    tablet_.SetCaptions(std::vector<std::string>({cap}));
   }
+
+  void AddRecord(int id, const std::vector<T> &values);
 
 private:
   Tablet tablet_;
+};
+
+template <typename T>
+struct ScalarReader {
+  ScalarReader(TabletReader&& reader) : reader_(reader) {}
+
+  std::vector<T> records() const;
+  std::vector<int> ids() const;
+  std::vector<int> timestamps() const;
+  std::vector<std::string> captions() const;
+  size_t size() const;
+
+ private:
+  TabletReader reader_;
 };
 
 }  // namespace components
