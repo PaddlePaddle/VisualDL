c65bf61b642eb79a2d8bc59bca3d80f45a6d3b84
superjom <superjom@gmail.com>
2017-12-25 09:56:21 +0800

    fix test

--- visualdl/python/test_summary.py
+++ visualdl/python/test_summary.py
@@ -7,15 +7,16 @@ import time
 class StorageTest(unittest.TestCase):
     def setUp(self):
         self.dir = "./tmp/storage_test"
-        self.writer = storage.StorageWriter(self.dir, sync_cycle=1).as_mode("train")
 
-    def test_write(self):
+    def test_read(self):
+        print 'test write'
+        self.writer = storage.StorageWriter(self.dir, sync_cycle=1).as_mode("train")
         scalar = self.writer.scalar("model/scalar/min")
-        scalar.set_caption("model/scalar/min")
+        # scalar.set_caption("model/scalar/min")
         for i in range(10):
             scalar.add_record(i, 1.0)
 
-    def test_read(self):
+        print 'test read'
         self.reader = storage.StorageReader("train", self.dir)
         scalar = self.reader.scalar("model/scalar/min")
         self.assertEqual(scalar.caption(), "train")
