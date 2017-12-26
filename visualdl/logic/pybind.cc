41c22081c20136af9d044934ac026b5d0e8872be
superjom <superjom@gmail.com>
2017-12-26 13:10:58 +0800

    add image py test

--- visualdl/logic/pybind.cc
+++ visualdl/logic/pybind.cc
@@ -11,28 +11,7 @@ namespace cp = visualdl::components;
 PYBIND11_PLUGIN(core) {
   py::module m("core", "C++ core of VisualDL");
 
-#define ADD_SCALAR(T)                                      \
-  py::class_<cp::ScalarReader<T>>(m, "ScalarReader__" #T)  \
-      .def("records", &cp::ScalarReader<T>::records)       \
-      .def("timestamps", &cp::ScalarReader<T>::timestamps) \
-      .def("ids", &cp::ScalarReader<T>::ids)               \
-      .def("caption", &cp::ScalarReader<T>::caption);
-  ADD_SCALAR(int);
-  ADD_SCALAR(float);
-  ADD_SCALAR(double);
-  ADD_SCALAR(int64_t);
-#undef ADD_SCALAR
-
-#define ADD_SCALAR_WRITER(T)                          \
-  py::class_<cp::Scalar<T>>(m, "ScalarWriter__" #T)   \
-      .def("set_caption", &cp::Scalar<T>::SetCaption) \
-      .def("add_record", &cp::Scalar<T>::AddRecord);
-  ADD_SCALAR_WRITER(int);
-  ADD_SCALAR_WRITER(float);
-  ADD_SCALAR_WRITER(double);
-#undef ADD_SCALAR_WRITER
-
-#define ADD_SCALAR(T)                                                   \
+#define READER_ADD_SCALAR(T)                                            \
   .def("get_scalar_" #T, [](vs::Reader& self, const std::string& tag) { \
     auto tablet = self.tablet(tag);                                     \
     return vs::components::ScalarReader<T>(std::move(tablet));          \
@@ -46,13 +25,17 @@ PYBIND11_PLUGIN(core) {
       .def("modes", [](vs::Reader& self) { return self.storage().modes(); })
       .def("tags", &vs::Reader::tags)
       // clang-format off
-    ADD_SCALAR(float)
-    ADD_SCALAR(double)
-    ADD_SCALAR(int);
-// clang-format on
-#undef ADD_SCALAR
+      READER_ADD_SCALAR(float)
+      READER_ADD_SCALAR(double)
+      READER_ADD_SCALAR(int)
+      // clang-format on
+      .def("get_image", [](vs::Reader& self, const std::string& tag) {
+        auto tablet = self.tablet(tag);
+        return vs::components::ImageReader(self.mode(), tablet);
+      });
+#undef READER_ADD_SCALAR
 
-#define ADD_SCALAR(T)                                                   \
+#define WRITER_ADD_SCALAR(T)                                            \
   .def("new_scalar_" #T, [](vs::Writer& self, const std::string& tag) { \
     auto tablet = self.AddTablet(tag);                                  \
     return cp::Scalar<T>(tablet);                                       \
@@ -65,10 +48,50 @@ PYBIND11_PLUGIN(core) {
            })
       .def("as_mode", &vs::Writer::AsMode)
       // clang-format off
-      ADD_SCALAR(float)
-      ADD_SCALAR(double)
-      ADD_SCALAR(int);
-// clang-format on
-#undef ADD_SCALAR
+      WRITER_ADD_SCALAR(float)
+      WRITER_ADD_SCALAR(double)
+      WRITER_ADD_SCALAR(int)
+      // clang-format on
+      .def("new_image",
+           [](vs::Writer& self, const std::string& tag, int num_samples) {
+             auto tablet = self.AddTablet(tag);
+             return vs::components::Image(tablet, num_samples);
+           });
+
+//------------------- components --------------------
+#define ADD_SCALAR_READER(T)                               \
+  py::class_<cp::ScalarReader<T>>(m, "ScalarReader__" #T)  \
+      .def("records", &cp::ScalarReader<T>::records)       \
+      .def("timestamps", &cp::ScalarReader<T>::timestamps) \
+      .def("ids", &cp::ScalarReader<T>::ids)               \
+      .def("caption", &cp::ScalarReader<T>::caption);
+  ADD_SCALAR_READER(int);
+  ADD_SCALAR_READER(float);
+  ADD_SCALAR_READER(double);
+  ADD_SCALAR_READER(int64_t);
+#undef ADD_SCALAR_READER
+
+#define ADD_SCALAR_WRITER(T)                          \
+  py::class_<cp::Scalar<T>>(m, "ScalarWriter__" #T)   \
+      .def("set_caption", &cp::Scalar<T>::SetCaption) \
+      .def("add_record", &cp::Scalar<T>::AddRecord);
+  ADD_SCALAR_WRITER(int);
+  ADD_SCALAR_WRITER(float);
+  ADD_SCALAR_WRITER(double);
+#undef ADD_SCALAR_WRITER
+
+  // clang-format on
+  py::class_<cp::Image>(m, "ImageWriter")
+      .def("set_caption", &cp::Image::SetCaption)
+      .def("start_sampling", &cp::Image::StartSampling)
+      .def("is_sample_taken", &cp::Image::IsSampleTaken)
+      .def("finish_sampling", &cp::Image::FinishSampling)
+      .def("set_sample", &cp::Image::SetSample);
+
+  py::class_<cp::ImageReader>(m, "ImageReader")
+      .def("caption", &cp::ImageReader::caption)
+      .def("num_records", &cp::ImageReader::num_records)
+      .def("data", &cp::ImageReader::data)
+      .def("shape", &cp::ImageReader::shape);
 
 }  // end pybind
--- visualdl/python/test_storage.py
+++ visualdl/python/test_storage.py
@@ -8,11 +8,11 @@ import time
 class StorageTest(unittest.TestCase):
     def setUp(self):
         self.dir = "./tmp/storage_test"
-
-    def test_read(self):
-        print 'test write'
         self.writer = storage.StorageWriter(
             self.dir, sync_cycle=1).as_mode("train")
+
+    def test_scalar(self):
+        print 'test write'
         scalar = self.writer.scalar("model/scalar/min")
         # scalar.set_caption("model/scalar/min")
         for i in range(10):
@@ -28,6 +28,27 @@ class StorageTest(unittest.TestCase):
         self.assertTrue(np.equal(ids, [float(i) for i in range(10)]).all())
         print 'records', records
         print 'ids', ids
+
+    def test_image(self):
+        tag = "layer1/layer2/image0"
+        image_writer = self.writer.image(tag, 10)
+        num_passes = 10
+        num_samples = 100
+
+        for pass_ in xrange(num_passes):
+            image_writer.start_sampling()
+            for ins in xrange(num_samples):
+                index =  image_writer.is_sample_taken()
+                if index != -1:
+                    shape = [3, 10, 10]
+                    data = np.random.random(shape) * 256
+                    data = np.ndarray.flatten(data)
+                    image_writer.set_sample(index, shape, list(data))
+            image_writer.finish_sampling()
+
+        self.reader = storage.StorageReader(self.dir).as_mode("train")
+        image_reader = self.reader.image(tag)
+        self.assertEqual(image_reader.caption(), tag)
 
 
 if __name__ == '__main__':
