d91cfe66343ff27ce605e88db144612806632ac3
qiaolongfei <qiaolongfei@baidu.com>
2017-12-27 15:33:16 +0800

    update setup.py

--- server/build.sh
+++ server/build.sh
@@ -1,2 +1,7 @@
-protoc visualdl/onnx/onnx.proto --python_out .
-python setup.py bdist_wheel
+script=$(readlink -f "$0")
+script_path=$(dirname "$script")
+
+pushd $script_path
+  protoc visualdl/onnx/onnx.proto --python_out .
+  python setup.py bdist_wheel
+popd
