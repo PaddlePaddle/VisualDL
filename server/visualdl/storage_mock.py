2e3eca35f61df1a53bc8dbd3bfa4b3ab00648804
superjom <superjom@gmail.com>
2017-12-28 13:53:00 +0800

    add server test

--- server/visualdl/storage_mock.py
+++ server/visualdl/storage_mock.py
@@ -4,46 +4,45 @@ import unittest
 
 import numpy as np
 
-import storage
 
-dir = "./tmp/mock"
-writer = storage.StorageWriter(dir, sync_cycle=20)
+def add_scalar(writer, mode, tag, num_steps, skip):
+    my_writer = writer.as_mode(mode)
+    scalar = my_writer.scalar(tag)
+    for i in range(num_steps):
+        if i % skip == 0:
+            scalar.add_record(i, random.random())
 
-train_writer = writer.as_mode("train")
-test_writer = writer.as_mode("test")
 
-train_scalar = train_writer.scalar("model/scalar/min")
-test_scalar = test_writer.scalar("model/scalar/min")
-
-train_scalar1 = train_writer.scalar("model/scalar/max")
-test_scalar1 = test_writer.scalar("model/scalar/max")
-
-for i in range(100):
-    train_scalar.add_record(i, random.random())
-    train_scalar1.add_record(i, random.random())
-    if i % 10 == 0:
-        test_scalar.add_record(i, random.random())
-        test_scalar1.add_record(i, random.random())
-
-def add_image(mode):
+def add_image(writer,
+              mode,
+              tag,
+              num_samples,
+              num_passes,
+              step_cycle,
+              shape=[50, 50, 3]):
     writer_ = writer.as_mode(mode)
-    tag = "layer1/layer2/image0"
-    # TODO check step_cycle
-    num_samples = 10
-    num_passes = 20
-    image_writer = writer_.image(tag, num_samples, 1)
-    shape = [50, 50, 3]
+    image_writer = writer_.image(tag, num_samples, step_cycle)
 
     for pass_ in xrange(num_passes):
         image_writer.start_sampling()
-        for ins in xrange(2*num_samples):
+        for ins in xrange(2 * num_samples):
             print '.',
-            index =  image_writer.is_sample_taken()
+            index = image_writer.is_sample_taken()
             if index != -1:
                 data = np.random.random(shape) * 256
                 data = np.ndarray.flatten(data)
                 image_writer.set_sample(index, shape, list(data))
         image_writer.finish_sampling()
 
-add_image("train")
-add_image("test")
+
+if __name__ == '__main__':
+    add_scalar("train", "layer/scalar0/min", 1000, 1)
+    add_scalar("test", "layer/scalar0/min", 1000, 10)
+    add_scalar("valid", "layer/scalar0/min", 1000, 10)
+
+    add_scalar("train", "layer/scalar0/max", 1000, 1)
+    add_scalar("test", "layer/scalar0/max", 1000, 10)
+    add_scalar("valid", "layer/scalar0/max", 1000, 10)
+
+    add_image("train", "layer/image0", 7, 10, 1)
+    add_image("test", "layer/image0", 7, 10, 3)
