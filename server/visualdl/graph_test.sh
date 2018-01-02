63d22ea93100a4d2dd4892f5a3482ceab6c9e523
daming-lu <daminglu@yahoo.com>
2018-01-02 21:57:25 +0800

    added protc, should work

--- server/visualdl/graph_test.sh
+++ server/visualdl/graph_test.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -ex
 
-CORE_PATH=$1
-
 cd mock
 bash download_mock_models.sh
 
 cd ..
-
-cp "$CORE_PATH/onnx_pb2.py" onnx
 
 python graph_test.py
 
--- tests.sh
+++ tests.sh
@@ -32,18 +32,20 @@ server_test() {
 
     curl -OL https://github.com/google/protobuf/releases/download/v3.1.0/protoc-3.1.0-linux-x86_64.zip
     unzip protoc-3.1.0-linux-x86_64.zip -d protoc3
-    sudo mv protoc3/bin/* /usr/local/bin/
-    sudo mv protoc3/include/* /usr/local/include/
-    sudo chown `whoami` /usr/local/bin/protoc
-    sudo chown -R `whoami` /usr/local/include/google
+    export PAHT=$PATH:protoc3/bin
+    sudo chown `whoami` protoc3/bin/protoc
 
     cd $cur/server
-    bash build.sh $core_path
-    cd visualdl
+    bash build.sh
 
-    bash graph_test.sh $core_path
-#    cd $cur/server/visualdl
-#    python lib_test.py
+    cd visualdl
+    bash graph_test.sh
+
+    cd $cur/server/visualdl
+    python lib_test.py
+
+    rm protoc-3.1.0-linux-x86_64.zip
+    rm -rf protoc3
 }
 
 echo "mode" $mode
@@ -51,8 +53,8 @@ echo "mode" $mode
 if [ $mode = "backend" ]; then
     backend_test
 elif [ $mode = "all" ]; then
-#    frontend_test
-#    backend_test
+    frontend_test
+    backend_test
     server_test
 else
     frontend_test
