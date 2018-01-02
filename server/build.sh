eeeb9fa4b0e084c0034c4c66c4d746e30c01d4bf
daming-lu <daminglu@yahoo.com>
2018-01-02 22:37:00 +0800

    does it exist?

--- server/build.sh
+++ server/build.sh
@@ -3,5 +3,15 @@ script_path=$(dirname "$script")
 
 pushd $script_path
   protoc visualdl/onnx/onnx.proto --python_out .
+  pb_file="visualdl/onnx/onnx_pb2.py"
+  if [ -f "$pb_file" ]
+  then
+    echo 'exist!!'
+    ls $pb_file
+    echo '$$CORE_PATH'
+    cp $pb_file $CORE_PATH
+  else
+    echo 'no!!'
+  fi
   python setup.py bdist_wheel
 popd
