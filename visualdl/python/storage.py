643f725e097c8ec980ca1e3916c4eb6b9ee84162
superjom <superjom@gmail.com>
2018-01-02 09:59:55 +0800

    add with syntax

--- visualdl/python/storage.py
+++ visualdl/python/storage.py
@@ -9,9 +9,15 @@ dtypes = ("float", "double", "int32", "int64")
 
 class StorageReader(object):
 
+    cur_mode = None
+
     def __init__(self, dir, reader=None):
         self.dir = dir
         self.reader = reader if reader else core.Reader(dir)
+
+    def mode(self, mode):
+        StorageReader.cur_mode = self.as_mode(mode)
+        return StorageReader.cur_mode
 
     def as_mode(self, mode):
         tmp = StorageReader(dir, self.reader.as_mode(mode))
@@ -34,17 +40,29 @@ class StorageReader(object):
     def image(self, tag):
         return self.reader.get_image(tag)
 
+    def __enter__(self):
+        return StorageReader.cur_mode
+
+    def __exit__(self, type, value, traceback):
+        pass
+
 
 class StorageWriter(object):
+
+    cur_mode = None
 
     def __init__(self, dir, sync_cycle, writer=None):
         self.dir = dir
         self.sync_cycle = sync_cycle
         self.writer = writer if writer else core.Writer(dir, sync_cycle)
 
+    def mode(self, mode):
+        StorageWriter.cur_mode = self.as_mode(mode)
+        return StorageWriter.cur_mode
+
     def as_mode(self, mode):
-        tmp = StorageWriter(self.dir, self.sync_cycle, self.writer.as_mode(mode))
-        return tmp
+        StorageWriter.cur_mode = StorageWriter(self.dir, self.sync_cycle, self.writer.as_mode(mode))
+        return StorageWriter.cur_mode
 
     def scalar(self, tag, type='float'):
         type2scalar = {
@@ -56,3 +74,9 @@ class StorageWriter(object):
 
     def image(self, tag, num_samples, step_cycle):
         return self.writer.new_image(tag, num_samples, step_cycle)
+
+    def __enter__(self):
+        return StorageWriter.cur_mode
+
+    def __exit__(self, type, value, traceback):
+        pass
