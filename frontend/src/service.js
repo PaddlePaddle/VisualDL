2d61c8f847120ac8a29d80663e9255f9ea32568b
BingBlog <abingblog@gmail.com>
2018-01-03 10:57:19 +0800

    add histograms

--- frontend/src/service.js
+++ frontend/src/service.js
@@ -9,3 +9,8 @@ export const getPluginScalarsScalars = makeService('/data/plugin/scalars/scalars
 export const getPluginImagesTags = makeService('/data/plugin/images/tags');
 
 export const getPluginImagesImages = makeService('/data/plugin/images/images');
+
+export const getPluginHistogramsTags = makeService('/data/plugin/histograms/tags');
+
+export const getPluginHistogramsHistograms = makeService('/data/plugin/histograms/histograms');
+
