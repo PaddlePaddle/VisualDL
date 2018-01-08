5272ee0ae6294184db46ad8d9eee5b3be2a3f4da
Yan Chunwei <yanchunwei@outlook.com>
2018-01-08 14:28:37 +0800

    feature/pip build (#64)

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -ex
+
+TOP_DIR=$(pwd)
+FRONTEND_DIR=$TOP_DIR/frontend
+BACKEND_DIR=$TOP_DIR/visualdl
+BUILD_DIR=$TOP_DIR/build
+
+mkdir -p $BUILD_DIR
+
+build_frontend() {
+    cd $FRONTEND_DIR
+    if [ ! -d "dist" ]; then
+      npm install
+      npm run build
+    fi
+}
+
+build_frontend_fake() {
+    cd $FRONTEND_DIR
+    mkdir -p dist
+}
+
+build_backend() {
+    cd $BUILD_DIR
+    cmake ..
+    make -j2
+}
+
+build_onnx_graph() {
+    # TODO(ChunweiYan) check protoc version here
+    cd $TOP_DIR/visualdl/server/onnx
+    protoc onnx.proto --python_out .
+}
+
+package() {
+    cp -rf $FRONTEND_DIR/dist $TOP_DIR/visualdl/server/
+    cp $BUILD_DIR/visualdl/logic/core.so $TOP_DIR/visualdl
+    cp $BUILD_DIR/visualdl/logic/core.so $TOP_DIR/visualdl/python/
+}
+
+ARG=$1
+echo "ARG: " $ARG
+
+
+if [ $ARG = "travis-CI" ]; then
+    build_frontend_fake
+else
+    build_frontend
+fi
+
+build_backend
+build_onnx_graph
+package
