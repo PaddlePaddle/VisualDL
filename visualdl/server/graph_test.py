51940612316bd6a15297cd6b3e8942b27047159b
qiaolongfei <qiaolongfei@baidu.com>
2018-01-08 15:40:02 +0800

    fix graph_test.py

--- visualdl/server/graph_test.py
+++ visualdl/server/graph_test.py
@@ -38,8 +38,7 @@ class GraphTest(unittest.TestCase):
         self.assertEqual(json_obj['edges'][111]['label'], 'label_111')
 
     def test_graph_edges_inception_v1(self):
-        json_str = graph.load_model(self.mock_dir + '/inception_v1_model.pb')
-        json_obj = json.loads(json_str)
+        json_obj = graph.to_IR_json(self.mock_dir + '/inception_v1_model.pb')
 
         # 286 edges + 143 nodes (out-edge of each node is counted twice)
         self.assertEqual(len(json_obj['edges']), 286 + 143)
