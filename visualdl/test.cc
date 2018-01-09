04e3f0fc2b48e455d6925e1ca1fc3271aaf705f3
Yan Chunwei <yanchunwei@outlook.com>
2018-01-09 12:54:01 +0800

    Revert "feature/glog to exception" (#72)

--- visualdl/test.cc
+++ visualdl/test.cc
@@ -1,9 +1,6 @@
 #include "gtest/gtest.h"
 
-#include "visualdl/utils/logging.h"
-
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  std::signal(SIGINT, visualdl::log::SignalHandler);
   return RUN_ALL_TESTS();
 }
