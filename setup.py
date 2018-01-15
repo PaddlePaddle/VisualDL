14829e5864de1e904a80d648d103b32c81da4b3d
superjom <superjom@gmail.com>
2018-01-15 14:15:05 +0800

    add demo

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
     install_requires=install_requires,
     package_data={'visualdl.server': datas,
                   'visualdl':['core.so'],
-                  'visualdl.python':['core.so']},
+                  'visualdl.python':['core.so', 'dog.jpg']},
     packages=packages,
-    scripts=['visualdl/server/visualDL'],
+    scripts=['visualdl/server/visualDL', 'demo/vdl_scratch.py'],
     cmdclass=cmdclass)
