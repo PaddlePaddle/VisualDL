1570b9bac05555a023aae559b07cb09babdb31d3
BingBlog <abingblog@gmail.com>
2018-01-18 16:44:02 +0800

    fix image not show slide

--- frontend/src/common/component/Notification/NotificationItem.js
+++ frontend/src/common/component/Notification/NotificationItem.js
@@ -16,15 +16,15 @@ export default class NotificationItem extends Component {
         on-mouseenter="clearTimer"
         on-mouseleave="startTimer"
         on-click="handleClick">
-        <s-icon
+        <san-icon
             s-if="type"
             class="sm-notification-type type-{{type}}"
-            size="50">{{iconType}}</s-icon>
+            size="50">{{iconType}}</san-icon>
         <div class="sm-notification-group" >
             <p class="sm-notification-title">{{title}}</p>
             <div class="sm-notification-content">{{message}}</div>
             <div on-click="close($event)" class="sm-notification-close">
-                <s-icon size="20" class="sm-notification-close-btn">close</s-icon>
+                <san-icon size="20" class="sm-notification-close-btn">close</san-icon>
             </div>
         </div>
     </div>
