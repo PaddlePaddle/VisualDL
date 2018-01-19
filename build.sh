b5d2df9543913cb6237c2b4e723c5b79bdde7679
Qiao Longfei <qiaolongfei@baidu.com>
2018-01-19 21:15:52 +0800

    Fix build (#192)
    * fix build problem that the first build has no js.
    * use for loop to check if frontend file is duplicated
    * package_data use *.js *.html to match file

--- build.sh
+++ build.sh
@@ -8,12 +8,25 @@ BUILD_DIR=$TOP_DIR/build
 
 mkdir -p $BUILD_DIR
 
+check_duplicated() {
+    filename_format=$1
+    file_num=`ls dist/${filename_format} | wc -l | awk '{$1=$1;print}'`
+    if [ "$file_num" != "1" ]; then
+      echo "dist have duplicate file for $file_num, please clean and rerun"
+      exit 1
+    fi
+}
+
 build_frontend() {
     cd $FRONTEND_DIR
     if [ ! -d "dist" ]; then
       npm install
       npm run build
     fi
+    for file_name in "manifest.*.js" "index.*.js" "vendor.*.js"; do
+        echo $file_name
+        check_duplicated $file_name
+    done
 }
 
 build_frontend_fake() {
@@ -43,6 +56,14 @@ build_onnx_graph() {
     protoc onnx.proto --python_out .
 }
 
+clean_env() {
+    rm -rf $TOP_DIR/visualdl/server/dist
+    rm -rf $BUILD_DIR/bdist*
+    rm -rf $BUILD_DIR/lib*
+    rm -rf $BUILD_DIR/temp*
+    rm -rf $BUILD_DIR/scripts*
+}
+
 package() {
     cp -rf $FRONTEND_DIR/dist $TOP_DIR/visualdl/server/
     cp $BUILD_DIR/visualdl/logic/core.so $TOP_DIR/visualdl
@@ -53,12 +74,13 @@ ARG=$1
 echo "ARG: " $ARG
 
 
-if [ $ARG = "travis-CI" ]; then
+if [ "$ARG" = "travis-CI" ]; then
     build_frontend_fake
 else
     build_frontend
 fi
 
+clean_env
 build_backend
 build_onnx_graph
 package
