1fb2c4245fc0418d9b794f223970ea0ec4b55e36
Wang,Jeff <wangjeff@baidu.com>
2018-02-01 11:02:31 -0800

    tweak the script so the user doesn't need to specify abs path to the logdir.

--- scripts/start_dev_server.sh
+++ scripts/start_dev_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+CURRENT_DIR=`pwd`
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPT_DIR/../frontend
@@ -8,5 +10,5 @@ cd $SCRIPT_DIR/../frontend
 
 export PYTHON_PATH=$PYTHON_PATH:"$SCRIPT_DIR/.."
 
-cd $SCRIPT_DIR/../visualdl/server
-python visualDL "$@"
+cd $CURRENT_DIR
+python ${SCRIPT_DIR}/../visualdl/server/visualDL "$@"
