c5de73a04f5dcf6bef554222e3abfa20204496c2
Thuan Nguyen <cs2be@yahoo.com>
2018-01-31 17:06:23 -0800

    remove npm install

--- scripts/watch_frontend.sh
+++ scripts/watch_frontend.sh
@@ -4,5 +4,4 @@ set -ex
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPT_DIR/../frontend
-npm install
 ./node_modules/.bin/webpack --watch --config tool/webpack.dev.config.js --output-path=../visualdl/server/dist
