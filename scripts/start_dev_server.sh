1ee852aefe519513a0f62d576edbf1a1aee698c1
Wang,Jeff <wangjeff@baidu.com>
2018-02-05 11:52:18 -0800

    Switch to use trap

--- scripts/start_dev_server.sh
+++ scripts/start_dev_server.sh
@@ -6,20 +6,18 @@ CURRENT_DIR=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPT_DIR/../frontend
+export PYTHONPATH=$PYTHONPATH:"$SCRIPT_DIR/.."
 
 ./node_modules/.bin/webpack --watch --config tool/webpack.dev.config.js --output-path=../visualdl/server/dist &
 # Track webpack pid
 WEBPACKPID=$!
 
-export PYTHONPATH=$PYTHONPATH:"$SCRIPT_DIR/.."
-
-cd $CURRENT_DIR
-
-{
-    # Run the visualDL with local PATH
-    python ${SCRIPT_DIR}/../visualdl/server/visualDL "$@"
-} ||
-{
-    # Catch python error, clean up the webpack process.
+function finish {
     kill -9 $WEBPACKPID
 }
+
+trap finish EXIT
+cd $CURRENT_DIR
+
+#Run the visualDL with local PATH
+python ${SCRIPT_DIR}/../visualdl/server/visualDL "$@"
