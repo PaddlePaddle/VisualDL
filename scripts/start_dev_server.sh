863a1dbca0fa317a76eaac4a23da5b3249938c78
Wang,Jeff <wangjeff@baidu.com>
2018-02-05 11:17:06 -0800

    Fix python path typo in the start dev server script Kill webpack in case of python error.

--- scripts/start_dev_server.sh
+++ scripts/start_dev_server.sh
@@ -6,9 +6,20 @@ CURRENT_DIR=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPT_DIR/../frontend
-./node_modules/.bin/webpack --watch --config tool/webpack.dev.config.js --output-path=../visualdl/server/dist &
 
-export PYTHON_PATH=$PYTHON_PATH:"$SCRIPT_DIR/.."
+./node_modules/.bin/webpack --watch --config tool/webpack.dev.config.js --output-path=../visualdl/server/dist &
+# Track webpack pid
+WEBPACKPID=$!
+
+export PYTHONPATH=$PYTHONPATH:"$SCRIPT_DIR/.."
 
 cd $CURRENT_DIR
-python ${SCRIPT_DIR}/../visualdl/server/visualDL "$@"
+
+{
+    # Run the visualDL with local PATH
+    python ${SCRIPT_DIR}/../visualdl/server/visualDL "$@"
+} ||
+{
+    # Catch python error, clean up the webpack process.
+    kill -9 $WEBPACKPID
+}
