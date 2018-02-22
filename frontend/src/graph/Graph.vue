ff5015266270cd21a8cf7e1672c3430a6a4803fa
Wang,Jeff <wangjeff@baidu.com>
2018-02-22 15:56:09 -0800

    Add the Graph.vue and the Config.vue for Graph tab

--- frontend/src/graph/Graph.vue
+++ frontend/src/graph/Graph.vue
@@ -1,29 +1,56 @@
 <template>
     <div class="visual-dl-page-container">
-        <div>I AM GRAPH</div>
-
         <div class="visual-dl-page-left">
-            <p>
-                Download
-            </p>
+            <ui-chart
+                :fitScreen="fitScreen"
+                :download="download"
+                :scale="config.scale"
+            ></ui-chart>
         </div>
         <div class="visual-dl-page-right">
             <div class="visual-dl-page-config-container">
-                <p>
-                    Show config
-                </p>
+                <ui-config
+                    :config="config"
+                    @fitScreen="handleFitScreen"
+                    @download="handleDownload"
+                ></ui-config>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import autoAdjustHeight from '../common/util/autoAdjustHeight';
+import Config from './ui/Config'
+import Chart from './ui/Chart';
+
 export default {
-    name: 'Graph ',
+    components: {
+        'ui-config': Config,
+        'ui-chart': Chart
+    },
+    name: 'Graph',
     data () {
         return {
+            config: {
+                scale: 0.5
+            },
+            fitScreen: {fitScreen: false},
+            download: {download: false}
         }
     },
+    mounted() {
+        autoAdjustHeight();
+    },
+    methods: {
+        handleFitScreen() {
+            this.fitScreen = {fitScreen: true}
+            this.config.scale = 0.5;
+        },
+        handleDownload() {
+            this.download = {fitScreen: true}
+        }
+    }
 };
 
 </script>
