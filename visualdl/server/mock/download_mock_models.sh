aced4c1b82635c656745aaa9ca79b2af6cbfd95c
daming-lu <daminglu@yahoo.com>
2018-04-16 14:00:37 -0700

    change squeezenet model from Baidu cloud to Dropbox

--- visualdl/server/mock/download_mock_models.sh
+++ visualdl/server/mock/download_mock_models.sh
@@ -1,6 +1,6 @@
 # Download squeezenet model
-curl -LOk http://visualdl.bj.bcebos.com/squeezenet.tar.gz
-#curl -LOk https://www.dropbox.com/s/fip3jzxsjf2g6zc/squeezenet.tar.gz?dl=0
+#curl -LOk http://visualdl.bj.bcebos.com/squeezenet.tar.gz
+curl -LOk https://www.dropbox.com/s/fip3jzxsjf2g6zc/squeezenet.tar.gz?dl=0
 tar -xvzf squeezenet.tar.gz
 cp squeezenet/model.pb squeezenet_model.pb
 
