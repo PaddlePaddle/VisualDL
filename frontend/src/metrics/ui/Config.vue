2d88e715e196d4b9b8db5fd2a750b545de67320f
Nicky Chan <nicky@baidu.com>
2018-09-06 16:02:51 -0700

    Redesign config for metrics combined scalar and histogram (#495)

--- frontend/src/metrics/ui/Config.vue
+++ frontend/src/metrics/ui/Config.vue
@@ -7,47 +7,81 @@
       dark
     />
 
-    <div class="visual-dl-page-slider-block">
-      <v-slider
-        label="Smoothing"
-        :max="0.99"
-        :min="0"
-        :step="0.01"
-        v-model="smoothingValue"
-        class="visual-dl-page-smoothing-slider"
-        dark/>
-      <span class="visual-dl-page-slider-span">{{ smoothingValue }}</span>
+    <v-checkbox
+      class="visual-dl-page-config-checkbox"
+      label="Scalars"
+      v-model="config.scalar.display"
+      :disabled="!config.scalar.enabled"
+      dark/>
+
+    <div class="visual-dl-page-component-block">
+
+        <div class="visual-dl-page-control-block">
+          <span :class="'visual-dl-page-control-span' + (config.scalar.display ? '' : ' visual-dl-page-disabled-text')">Smoothing</span>
+          <v-slider
+            :max="0.99"
+            :min="0"
+            :step="0.01"
+            v-model="smoothingValue"
+            class="visual-dl-page-smoothing-slider"
+            dark
+            :disabled="!config.scalar.display"/>
+          <span :class="'visual-dl-page-slider-span' + (config.scalar.display ? '' : ' visual-dl-page-disabled-text')">{{ smoothingValue }}</span>
+        </div>
+
+        <div class="visual-dl-page-control-block">
+            <span :class="'visual-dl-page-control-span' + (config.scalar.display ? '' : ' visual-dl-page-disabled-text')">X-axis</span>
+            <v-select
+              :items="horizontalItems"
+              v-model="config.horizontal"
+              class="visual-dl-page-config-selector"
+              dark
+              dense
+              :disabled="!config.scalar.display"
+            />
+        </div>
+
+        <div class="visual-dl-page-control-block">
+            <span :class="'visual-dl-page-control-span' + (config.scalar.display ? '' : ' visual-dl-page-disabled-text')">Tooltip sorting</span>
+            <v-select
+              :items="sortingMethodItems"
+              v-model="config.sortingMethod"
+              class="visual-dl-page-config-selector"
+              dark
+              dense
+              :disabled="!config.scalar.display"
+            />
+        </div>
+
+        <v-checkbox
+          class="visual-dl-page-outliers-checkbox"
+          label="Ignore outliers in chart scaling"
+          v-model="config.outlier"
+          dark
+          :disabled="!config.scalar.display"/>
+
     </div>
-
-    <v-radio-group
-      label="X-Axis"
-      v-model="config.horizontal"
-      dark>
-      <v-radio
-        label="Step"
-        value="step"/>
-      <v-radio
-        label="Relative"
-        value="relative"/>
-      <v-radio
-        label="Wall Time"
-        value="wall"/>
-    </v-radio-group>
-
-    <v-select
-      :items="sortingMethodItems"
-      v-model="config.sortingMethod"
-      label="Tooltip sorting method"
-      class="visual-dl-page-config-selector"
-      dark
-      dense
-    />
 
     <v-checkbox
       class="visual-dl-page-config-checkbox"
-      label="Ignore outliers in chart scaling"
-      v-model="config.outlier"
+      label="Histogram"
+      v-model="config.histogram.display"
+      :disabled="!config.histogram.enabled"
       dark/>
+
+      <div class="visual-dl-page-component-block">
+         <div class="visual-dl-page-control-block">
+           <span :class="'visual-dl-page-control-span' + (config.histogram.display ? '' : ' visual-dl-page-disabled-text')">Mode</span>
+           <v-select
+              :items="chartTypeItems"
+              v-model="config.chartType"
+              class="visual-dl-page-config-selector"
+              dark
+              dense
+              :disabled="!config.histogram.display"
+            />
+        </div>
+      </div>
 
     <v-btn
       :color="config.running ? 'primary' : 'error'"
@@ -75,20 +109,30 @@ export default {
     return {
       horizontalItems: [
         {
-          name: 'Step',
+          text: 'Step',
           value: 'step',
         },
         {
-          name: 'Relative',
+          text: 'Relative',
           value: 'relative',
         },
         {
-          name: 'Wall',
+          text: 'Wall Time',
           value: 'wall',
         },
       ],
       sortingMethodItems: [
         'default', 'descending', 'ascending', 'nearest',
+      ],
+      chartTypeItems: [
+        {
+          text: 'Overlay',
+          value: 'overlay',
+        },
+        {
+          text: 'Offset',
+          value: 'offset',
+        },
       ],
       smoothingValue: this.config.smoothing,
       isDemo: process.env.NODE_ENV === 'demo',
@@ -98,7 +142,7 @@ export default {
     smoothingValue: _.debounce(
       function() {
         this.config.smoothing = this.smoothingValue;
-      }, 500
+      }, 50
     ),
   },
   methods: {
@@ -113,24 +157,37 @@ export default {
 +prefix-classes('visual-dl-page-')
     .config-com
         padding 20px
-        .slider-block
+        .component-block
+            padding-left 33px
+            padding-bottom 20px
+            margin-top -10px
+        .control-block
+            height 36px
             display flex
             align-items center
+        .control-span
+            font-size 12px
+            width 110px
+            margin-top 8px
+        .disabled-text
+            opacity 0.5
         .smoothing-slider
             display inline
         .slider-span
-            width 40px
+            width 35px
+            font-size 13px
         .run-toggle
             margin-top 20px
         .config-checkbox label
             font-size 13px
-        .config-selector
-            margin-top 12px
-            margin-bottom 20px
-        .checkbox-group-label
-            display flex
-            margin-top 20px
-            margin-bottom 10px
+            font-weight bold
+        .outliers-checkbox
+            margin-top 10px
+        .outliers-checkbox label
+            font-size 12px
+
+.input-group--select .input-group__selections__comma
+    font-size 12px
 
 </style>
 
