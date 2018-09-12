09fb162885e100e12aaf4caa5f18d2d15670aa76
Nicky <nicky@baidu.com>
2018-09-11 20:21:19 -0700

    Add a bar with different Group tag to select by users - Users can switch to different group tag by tab instead of using expand panel - Expand panel is confusing and difficult to see all group tags - Optimize performance by not showing duplicate instance of chart

---
+++ frontend/src/common/component/TagsTab.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    @click="$emit('click')"
+    class="visual-dl-tags-tab">
+    <span :class="active ? 'visual-dl-tags-tab-text-active':'visual-dl-tags-tab-text-inactive' ">{{ title }} &nbsp; ({{ total }})</span>
+    </h3>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="stylus">
+    .visual-dl-tags-tab
+      border solid 1px #ccc
+      line-height 40px
+      height 40px
+      padding 0 14px
+      margin-right 20px
+      cursor pointer
+      position relative
+      display inline-block
+      .visual-dl-tags-tab-text-active
+        font-size 13px
+        font-weight bold
+      .visual-dl-tags-tab-text-inactive
+        font-size 13px
+        font-weight normal
+
+</style>
+
+
