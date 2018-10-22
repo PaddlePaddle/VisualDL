6f9e50acc7e93f363532bcaa2865d049b5e7caaf
Wang,Jeff <wangjeff@baidu.com>
2018-10-22 14:14:29 -0700

    Update the travis script

--- scripts/deploy_docs_on_travis.sh
+++ scripts/deploy_docs_on_travis.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then exit 0; fi;
-if [[ "$TRAVIS_BRANCH" != "develop" ]]; then exit 0; fi;
-export DEPLOY_DOCS_SH=https://raw.githubusercontent.com/PaddlePaddle/PaddlePaddle.org/master/scripts/deploy/deploy_docs.sh
+
+if [ "$TRAVIS_BRANCH" == "develop_doc" ]; then
+    PPO_SCRIPT_BRANCH=develop
+elif [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+    PPO_SCRIPT_BRANCH=master
+else
+    exit 0;
+fi
+
+export DEPLOY_DOCS_SH=https://raw.githubusercontent.com/PaddlePaddle/PaddlePaddle.org/$PPO_SCRIPT_BRANCH/scripts/deploy/deploy_docs.sh
 export DOCS_DIR=`pwd`
 cd ..
-curl $DEPLOY_DOCS_SH | bash -s $CONTENT_DEC_PASSWD $TRAVIS_BRANCH $DOCS_DIR
+
+curl $DEPLOY_DOCS_SH | bash -s $CONTENT_DEC_PASSWD $TRAVIS_BRANCH $DOCS_DIR $DOCS_DIR/docs $PPO_SCRIPT_BRANCH
