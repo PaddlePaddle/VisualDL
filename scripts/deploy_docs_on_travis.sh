3639d7894385523bb7d22ed3a2d197e033b4329c
Varun Arora <aroravarun@baidu.com>
2018-12-18 17:50:03 -0800

    Fill small shell typo

--- scripts/deploy_docs_on_travis.sh
+++ scripts/deploy_docs_on_travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 exit_code=0
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then exit $exit_code=0; fi;
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then exit $exit_code; fi;
 
 if [ "$TRAVIS_BRANCH" == "develop_doc" ]; then
     PPO_SCRIPT_BRANCH=develop
