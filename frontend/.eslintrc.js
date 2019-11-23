5c6e79b4122abe607924190903f81abe32363a6d
XinPing Wang <xinping.wang@hotmail.com>
2019-11-23 23:54:57 +0800

    Fix code style config not consistent (#564)

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,18 +6,31 @@ module.exports = {
     'plugin:vue/strongly-recommended',
   ],
   parserOptions: {
-    "sourceType": "module",
+    'sourceType': 'module',
   },
   rules: {
     // override/add rules settings here, such as:
+    'arrow-parens': ['error', 'as-needed'],
+    'brace-style': ['warn', 'stroustrup'],
+    'comma-dangle': ['error', 'never'],
+    'max-len': ['warn', 120],
+    'no-console': 'warn',
+    'operator-linebreak': ['error', 'before'],
+    'quote-props': ['warn', 'as-needed'],
+    'space-before-function-paren': ['error', {
+      'anonymous': 'always',
+      'named': 'never',
+      'asyncArrow': 'never'
+    }],
+    'space-infix-ops': 'error',
+    'space-unary-ops': ['warn', {'words': true, 'nonwords': false}],
     'vue/no-unused-vars': 'warn',
-    'max-len': ["warn", 120],
-    "vue/prop-name-casing": ["error"],
-    'vue/script-indent': 'error',
+    'vue/prop-name-casing': ['error'],
+    'vue/script-indent': ['error', 4],
 
     // The following rules should apply eventually. Turn them off for now
     // so we can have pre-commit running
     'no-invalid-this': 'off',
     'require-jsdoc': 'off',
   }
-}
+};
