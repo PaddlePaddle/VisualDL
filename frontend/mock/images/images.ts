37eeb92d35f16295a8c07273685c6edcd9875519
Peter Pan <littlepanzh@gmail.com>
2020-02-29 17:53:22 +0800

    fix mock data problem

--- frontend/mock/images/images.ts
+++ frontend/mock/images/images.ts
@@ -1,74 +1,42 @@
-/* eslint-disable @typescript-eslint/camelcase */
-
 export default [
     {
         wallTime: 1512549785.061623,
-        step: 60,
-        query: 'sample=0&index=0&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 60
     },
     {
         wallTime: 1512886109.672786,
-        step: 60,
-        query: 'sample=0&index=1&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 60
     },
     {
         wallTime: 1512886124.266915,
-        step: 210,
-        query: 'sample=0&index=2&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 210
     },
     {
         wallTime: 1512886138.898628,
-        step: 330,
-        query: 'sample=0&index=3&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 330
     },
     {
         wallTime: 1512886139.883663,
-        step: 340,
-        query: 'sample=0&index=4&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 340
     },
     {
         wallTime: 1512886147.195567,
-        step: 410,
-        query: 'sample=0&index=5&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 410
     },
     {
         wallTime: 1512886156.47856,
-        step: 500,
-        query: 'sample=0&index=6&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 500
     },
     {
         wallTime: 1512886187.82793,
-        step: 810,
-        query: 'sample=0&index=7&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 810
     },
     {
         wallTime: 1512886200.386198,
-        step: 950,
-        query: 'sample=0&index=8&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 950
     },
     {
         wallTime: 1512886204.224405,
-        step: 990,
-        query: 'sample=0&index=9&tag=input_reshape%2Finput%2Fimage%2F0&run=test',
-        width: 28,
-        height: 28
+        step: 990
     }
 ];
