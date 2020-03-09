5662c86dbe3db872484f6613d3ee48352c62bb69
Peter Pan <littlepanzh@gmail.com>
2020-03-09 15:28:36 +0800

    build: optimize wasm files

--- frontend/scripts/build.sh
+++ frontend/scripts/build.sh
@@ -3,22 +3,46 @@
 set -e
 
 WORKING_PATH=`pwd`
-DIST_DIR='dist'
-DIST_PATH="$WORKING_PATH/$DIST_DIR"
+SERVER_DIR="dist"
+SERVER_DIR_PATH="$WORKING_PATH/$SERVER_DIR"
+CLIENT_DIR="out"
+CLIENT_DIR_PATH="$WORKING_PATH/$CLIENT_DIR"
+OUTPUT="output"
+OUTPUT_PATH="$WORKING_PATH/$OUTPUT"
 
 # generate dist
-rm -rf $DIST_PATH
-mkdir -p $DIST_PATH
+rm -rf $SERVER_DIR_PATH
+mkdir -p $SERVER_DIR_PATH
+rm -rf $CLIENT_DIR_PATH
+mkdir -p $CLIENT_DIR_PATH
 
-yarn
-# export
-# WARNING: export FIRST!!! dist files will be deleted by next after export
-yarn export
+# generate output
+rm -rf $OUTPUT_PATH
+mkdir -p $OUTPUT_PATH
+
 # next build
 yarn build:next
+
+# optimize wasm
+./scripts/optimize.sh
+
 # server build
 yarn build:server
 
 # move static files
-cp next.config.js $DIST_PATH
-cp package.json $DIST_PATH
+cp next.config.js $SERVER_DIR_PATH
+cp package.json $SERVER_DIR_PATH
+
+# package server files
+(cd $SERVER_DIR_PATH && tar zcf $OUTPUT_PATH/server.tar.gz .)
+
+# export
+# WARNING: export LAST!!! dist files will be deleted by next after export
+yarn export
+
+# package client files
+(cd $CLIENT_DIR_PATH && tar zcf $OUTPUT_PATH/client.tar.gz .)
+
+# clean
+rm -rf $SERVER_DIR_PATH
+rm -rf $CLIENT_DIR_PATH
---
+++ frontend/scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# rust toolchain
+# https://rustup.rs/
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
+source $HOME/.cargo/env
+
+# wasm-pack
+# https://rustwasm.github.io/wasm-pack/installer/
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+# binaryen
+# https://github.com/WebAssembly/binaryen
+mkdir build && cd build
+git clone https://github.com/WebAssembly/binaryen.git
+(cd binaryen && cmake . && make)
+export PATH="`pwd`/binaryn/bin:$PATH"
+cd ..
+
+yarn
