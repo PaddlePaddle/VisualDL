9d3dd4c1c38642a9e84434b0cea4d8b2899094b3
Peter Pan <littlepanzh@gmail.com>
2020-04-07 16:53:41 +0800

    build: add wine to build windows app in linux

--- frontend/scripts/install.sh
+++ frontend/scripts/install.sh
@@ -9,15 +9,32 @@ if ! hash rustup 2>/dev/null; then
     source $HOME/.cargo/env
 fi
 
+
 # wasm-pack
 # https://rustwasm.github.io/wasm-pack/installer/
 if ! hash wasm-pack 2>/dev/null; then
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 fi
 
+
+# wine
+sudo dpkg --add-architecture i386
+wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key
+sudo apt-key add Release.key
+sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./'
+
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+
+sudo apt update
+sudo apt install --install-recommends winehq-stable
+
+
 # yarn
 curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
 
 # yarn install
 yarn install --frozen-lockfile
