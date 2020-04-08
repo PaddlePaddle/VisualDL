6410aef1a5c0a6b6eb51fb1a7e5a5aa96945c742
Peter Pan <littlepanzh@gmail.com>
2020-04-08 19:01:39 +0800

    2.0.0-beta.29

--- frontend/scripts/install.sh
+++ frontend/scripts/install.sh
@@ -39,4 +39,4 @@ curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # yarn install
-yarn install --immutable
+yarn install --frozen-lockfile
