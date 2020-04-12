8e37f64a0cbfda8ed32e919604780c0a55e34fdc
Peter Pan <littlepanzh@gmail.com>
2020-04-13 01:49:32 +0800

    style: more type checks when commit

--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,13 +1,23 @@
 const path = require('path');
+const fs = require('fs');
 
 module.exports = {
-    '**/*.ts?(x)': async filenames =>
+    // lint all files when global package.json or eslint config changes.
+    './(package.json|.eslintrc.js)': () => 'yarn lint',
+
+    // check types when ts file or package.json changes.
+    'packages/**/(*.ts?(x)|package.json)': filenames =>
         [
             ...new Set(
                 filenames.map(
                     filename => path.relative(path.join(process.cwd(), 'packages'), filename).split(path.sep)[0]
                 )
             )
-        ].map(p => `tsc -p ${path.join(process.cwd(), 'packages', p, 'tsconfig.json')} --noEmit`),
+        ]
+            .map(p => path.join(process.cwd(), 'packages', p, 'tsconfig.json'))
+            .filter(p => fs.statSync(p).isFile())
+            .map(p => `tsc -p ${p} --noEmit`),
+
+    // lint changed files
     '**/*.(j|t)s?(x)': filenames => `eslint ${filenames.join(' ')}`
 };
