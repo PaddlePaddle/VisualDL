65c5b790795f9c9e9c47662f370ff84abe269f74
Peter Pan <littlepanzh@gmail.com>
2020-05-16 15:03:27 +0800

    feat: add tag select in high-dimensional page

---
+++ frontend/packages/mock/data/embeddings/tags.ts
@@ -0,0 +1,10 @@
+export default {
+    test: ['layer2/biases/summaries/mean', 'test/1234', 'another'],
+    train: [
+        'layer2/biases/summaries/mean',
+        'layer2/biases/summaries/accuracy',
+        'layer2/biases/summaries/cost',
+        'test/431',
+        'others'
+    ]
+};
