647858306f543a59ee9e7ca928d269f6e1845685
Peter Pan <littlepanzh@gmail.com>
2020-05-20 16:29:01 +0800

    feat: add --api-only argument

--- visualdl/server/app.py
+++ visualdl/server/app.py
@@ -55,17 +55,17 @@ class ParseArgs(object):
                  logdir,
                  host="0.0.0.0",
                  port=8040,
-                 model_pb="",
                  cache_timeout=20,
                  language=None,
-                 public_path=None):
+                 public_path=None,
+                 api_only=False):
         self.logdir = logdir
         self.host = host
         self.port = port
-        self.model_pb = model_pb
         self.cache_timeout = cache_timeout
         self.language = language
         self.public_path = public_path
+        self.api_only = api_only
 
 
 def try_call(function, *args, **kwargs):
@@ -122,8 +122,13 @@ def parse_args():
         "--public-path",
         type=str,
         action="store",
-        default="/app",
+        default="/",
         help="set public path"
+    )
+    parser.add_argument(
+        "--api-only",
+        action="store_true",
+        help="serve api only"
     )
 
     args = parser.parse_args()
@@ -164,8 +169,6 @@ def create_app(args):
     public_path = args.public_path.rstrip('/')
     api_path = public_path + '/api'
 
-    template = Template(os.path.join(server_path, template_file_path), PUBLIC_PATH=public_path.strip('/'))
-
     @babel.localeselector
     def get_locale():
         lang = args.language
@@ -173,27 +176,31 @@ def create_app(args):
             lang = request.accept_languages.best_match(support_language)
         return lang
 
-    @app.route("/")
-    def base():
-        return redirect(public_path, code=302)
+    if not args.api_only:
 
-    @app.route("/favicon.ico")
-    def favicon():
-        icon = os.path.join(template_file_path, 'favicon.ico')
-        if os.path.exists(icon):
-            return send_file(icon)
-        return "file not found", 404
+        template = Template(os.path.join(server_path, template_file_path), PUBLIC_PATH=public_path.strip('/'))
 
-    @app.route(public_path + "/")
-    def index():
-        lang = get_locale()
-        if lang == default_language:
-            return redirect(public_path + '/index', code=302)
-        return redirect(public_path + '/' + lang + '/index', code=302)
+        @app.route("/")
+        def base():
+            return redirect(public_path, code=302)
 
-    @app.route(public_path + '/<path:filename>')
-    def serve_static(filename):
-        return template.render(filename if re.search(r'\..+$', filename) else filename + '.html')
+        @app.route("/favicon.ico")
+        def favicon():
+            icon = os.path.join(template_file_path, 'favicon.ico')
+            if os.path.exists(icon):
+                return send_file(icon)
+            return "file not found", 404
+
+        @app.route(public_path + "/")
+        def index():
+            lang = get_locale()
+            if lang == default_language:
+                return redirect(public_path + '/index', code=302)
+            return redirect(public_path + '/' + lang + '/index', code=302)
+
+        @app.route(public_path + '/<path:filename>')
+        def serve_static(filename):
+            return template.render(filename if re.search(r'\..+$', filename) else filename + '.html')
 
     @app.route(api_path + "/components")
     def components():
@@ -336,19 +343,19 @@ def _open_browser(app, index_url):
 def _run(logdir,
          host="127.0.0.1",
          port=8080,
-         model_pb="",
          cache_timeout=20,
          language=None,
          public_path="/app",
+         api_only=False,
          open_browser=False):
     args = ParseArgs(
         logdir=logdir,
         host=host,
         port=port,
-        model_pb=model_pb,
         cache_timeout=cache_timeout,
         language=language,
-        public_path=public_path)
+        public_path=public_path,
+        api_only=api_only)
     logger.info(" port=" + str(args.port))
     app = create_app(args)
     index_url = "http://" + host + ":" + str(port) + args.public_path
@@ -362,19 +369,19 @@ def _run(logdir,
 def run(logdir,
         host="127.0.0.1",
         port=8040,
-        model_pb="",
         cache_timeout=20,
         language=None,
         public_path="/app",
+        api_only=False,
         open_browser=False):
     kwarg = {
         "logdir": logdir,
         "host": host,
         "port": port,
-        "model_pb": model_pb,
         "cache_timeout": cache_timeout,
         "language": language,
         "public_path": public_path,
+        "api_only": api_only,
         "open_browser": open_browser
     }
 
