5bb149e252664496ace64694785e6c15fcb5d852
Peter Pan <littlepanzh@gmail.com>
2020-08-24 20:57:26 +0800

    fix: PR Curve chart Y axis adaptive values (#756)

--- frontend/packages/core/resource/pr-curve/chart.ts
+++ frontend/packages/core/resource/pr-curve/chart.ts
@@ -5,13 +5,7 @@ export const options = {
     tooltip: {
         position: ['10%', '100%']
     },
-    xAxis: {
-        min: 0,
-        max: 1
-    },
     yAxis: {
-        min: 0,
-        max: 1,
         splitNumber: 5
     }
 };
