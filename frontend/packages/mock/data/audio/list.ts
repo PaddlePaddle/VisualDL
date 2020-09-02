4f1e4718f3c554bc41b5b9d4bd1b5de17ff43a07
Peter Pan <littlepanzh@gmail.com>
2020-09-03 00:20:00 +0800

    chore: use millisecond

--- frontend/packages/mock/data/audio/list.ts
+++ frontend/packages/mock/data/audio/list.ts
@@ -1,42 +1,42 @@
 export default [
     {
-        wallTime: 1512549785.061623,
+        wallTime: 1512549785061.623,
         step: 60
     },
     {
-        wallTime: 1512886109.672786,
+        wallTime: 1512886109672.786,
         step: 60
     },
     {
-        wallTime: 1512886124.266915,
+        wallTime: 1512886124266.915,
         step: 210
     },
     {
-        wallTime: 1512886138.898628,
+        wallTime: 1512886138898.628,
         step: 330
     },
     {
-        wallTime: 1512886139.883663,
+        wallTime: 1512886139883.663,
         step: 340
     },
     {
-        wallTime: 1512886147.195567,
+        wallTime: 1512886147195.567,
         step: 410
     },
     {
-        wallTime: 1512886156.47856,
+        wallTime: 1512886156478.56,
         step: 500
     },
     {
-        wallTime: 1512886187.82793,
+        wallTime: 1512886187827.93,
         step: 810
     },
     {
-        wallTime: 1512886200.386198,
+        wallTime: 1512886200386.198,
         step: 950
     },
     {
-        wallTime: 1512886204.224405,
+        wallTime: 1512886204224.405,
         step: 990
     }
 ];
