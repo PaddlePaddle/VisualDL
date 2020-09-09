8e562d4e5f27ac44aa4969669054bc21dd80038b
Peter Pan <littlepanzh@gmail.com>
2020-09-09 11:21:20 +0800

    minor bugs fix (#804)
    * fix: error message cannot display correctly
    
    * fix: query string missing when redirect from root to index

--- visualdl/server/app.py
+++ visualdl/server/app.py
@@ -69,6 +69,12 @@ def create_app(args):
     public_path = args.public_path
     api_path = public_path + '/api'
 
+    def append_query_string(url):
+        query_string = ''
+        if request.query_string:
+            query_string = '?' + request.query_string.decode()
+        return url + query_string
+
     @babel.localeselector
     def get_locale():
         lang = args.language
@@ -87,7 +93,7 @@ def create_app(args):
 
         @app.route('/')
         def base():
-            return redirect(public_path, code=302)
+            return redirect(append_query_string(public_path), code=302)
 
         @app.route('/favicon.ico')
         def favicon():
@@ -98,10 +104,7 @@ def create_app(args):
 
         @app.route(public_path + '/')
         def index():
-            query_string = ''
-            if request.query_string:
-                query_string = '?' + request.query_string.decode()
-            return redirect(public_path + '/index' + query_string, code=302)
+            return redirect(append_query_string(public_path + '/index'), code=302)
 
         @app.route(public_path + '/<path:filename>')
         def serve_static(filename):
