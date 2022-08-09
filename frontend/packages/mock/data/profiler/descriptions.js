bc8cc952cb05f39c631398cd955318f35fd5a592
xiaoyixin-cmd <1634228212@qq.com>
2022-08-09 17:55:03 +0800

    8-9 11

--- frontend/packages/mock/data/profiler/descriptions.js
+++
@@ -1,7 +0,0 @@
-export default {
-    overview_environment: 'CPU利用率：xxxxx\n.  GPU利用率：xxxxxx\n',
-    overview_model_perspective: 'xxxxxxxxxxxxxxxxxxxx',
-    overview_model_perspective_perstep: 'xxxxxxxxxxxxxxxxxxxx',
-    overview_event_type_perspective: 'xxxxxxxxxxxxxxxxxxxx',
-    overview_event_type_model_perspective: 'xxxxxxxxxxxxxxxxxxxx'
-};
--- frontend/packages/mock/data/profiler/timeunits.js
+++
@@ -1,1 +0,0 @@
-export default ['ns', 'us', 'ms', 's'];
