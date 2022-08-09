c1f49ad184f8e5384e6e97fc4497ca0481bb134a
chenjian <chenjian26@baidu.com>
2022-08-09 14:58:54 +0800

    fix const description

--- visualdl/component/profiler/parser/const_description.py
+++ visualdl/component/profiler/parser/const_description.py
@@ -23,90 +23,85 @@ __ALL__ = [
 ]
 
 TOOLTIP_DEVICE_INFO_CN = \
-  '<div class="bold">CPU进程利用率：</div>'\
-  '<div class="indent">进程所利用到的CPU的时间 / ProfileStep的时间(即性能分析的时间跨度）</div>'\
-  '<div class="bold">CPU系统利用率</div>'\
-  '<div class="indent">整个系统所有进程利用到的CPU时间 / CPU总时间（ProfileStep的时间*CPU核心数）</div>'\
-  '<div class="bold">GPU利用率：</div>'\
-  '<div class="indent">进程利用GPU计算的时间 / ProfileStep的时间，进程利用GPU计算的时间即是GPU Kernel计算的时间，越高越好</div>'\
-  '<div class="bold">流处理器效率：</div>'\
-  '<div class="indent">对于流处理器处理某个GPU Kernel, 其效率为SM_Eff_i = min(Kernel所用的Blocks数量 / GPU的流处理器数量, 100%)'\
-  '流处理器效率为SM_Eff_i关于每个Kernel的执行时间加权和 / ProfileStep的时间</div>'\
-  '<div class="bold">流处理器占用率：</div>'\
-  '<div class="indent">对于流处理器处理某个GPU Kernel, 其占用率Occu_i = 为活跃的warp数 / 能支持的最大warp数。流处理器占用率为\
-    Occu_i关于每个Kernel执行时间的加权平均</div>'\
-  '<div class="bold">Tensor cores使用时间占比：</div>'\
-  '<div class="indent">使用Tensor Cores的GPU Kernel的计算时间 / 所有Kernel的计算时间</div>'
+  '<b class="bold">CPU进程利用率：</b>'\
+  '&nbsp;&nbsp;进程所利用到的CPU的时间 / ProfileStep的时间(即性能分析的时间跨度）<br>'\
+  '<b class="bold">CPU系统利用率</b>'\
+  '&nbsp;&nbsp;整个系统所有进程利用到的CPU时间 / CPU总时间（ProfileStep的时间*CPU核心数）<br>'\
+  '<b class="bold">GPU利用率：</b>'\
+  '&nbsp;&nbsp;进程利用GPU计算的时间 / ProfileStep的时间，进程利用GPU计算的时间即是GPU Kernel计算的时间，越高越好<br>'\
+  '<b class="bold">流处理器效率：</b>'\
+  '&nbsp;&nbsp;对于流处理器处理某个GPU Kernel, 其效率为SM_Eff_i = min(Kernel所用的Blocks数量 / GPU的流处理器数量, 100%)'\
+  '流处理器效率为SM_Eff_i关于每个Kernel的执行时间加权和 / ProfileStep的时间<br>'\
+  '<b class="bold">流处理器占用率：</b>'\
+  '&nbsp;&nbsp;对于流处理器处理某个GPU Kernel, 其占用率Occu_i = 为活跃的warp数 / 能支持的最大warp数。流处理器占用率为Occu_i关于每个Kernel执行时间的加权平均<br>'\
+  '<b class="bold">Tensor cores使用时间占比：</b>'\
+  '&nbsp;&nbsp;使用Tensor Cores的GPU Kernel的计算时间 / 所有Kernel的计算时间<br>'
 
 TOOLTIP_MODEL_PERSPECTIVE_CN = \
-  '<div>展示模型各阶段DataLoader, Forward, Backward, Optimization以及Other的总CPU和GPU时间。</div>'\
-  '<div>CPU时间即是各阶段代码执行的时间，GPU时间是各阶段所调用的GPU Kernel在GPU上的计算时间。</div>'\
-  '<div class="indent"><div class="bold">DataLoader:</div> 表示使用paddle.io.DataLoader从数据集中取数据的阶段</div>'\
-  '<div class="indent"><div class="bold">Forward: </div> 表示模型前向计算的阶段</div>'\
-  '<div class="indent"><div class="bold">Backward: </div> 表示模型反向梯度计算的阶段</div>'\
-  '<div class="indent"><div class="bold">Optimization: </div> 表示模型优化更新参数的阶段</div>'\
-  '<div class="indent"><div class="bold">Other: </div> 其它时间</div>'
+  '展示模型各阶段DataLoader, Forward, Backward, Optimization以及Other的总CPU和GPU时间。<br>'\
+  'CPU时间即是各阶段代码执行的时间，GPU时间是各阶段所调用的GPU Kernel在GPU上的计算时间。<br>'\
+  '&nbsp;&nbsp;<b class="bold">DataLoader: 表示使用paddle.io.DataLoader从数据集中取数据的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Forward: 表示模型前向计算的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Backward: 表示模型反向梯度计算的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Optimization: 表示模型优化更新参数的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Other: 其它时间<br>'
 
 TOOLTIP_MODEL_PERSPECTIVE_PERSTEP_CN = \
-  '<div>展示每一个ProfileStep内模型各阶段DataLoader, Forward, Backward, Optimization以及Other的CPU和GPU时间。</div>'\
-  '<div>CPU时间即是各阶段代码执行的时间，GPU时间是各阶段所调用的GPU Kernel在GPU上的计算时间。</div>'\
-  '<div class="indent"><div class="bold">DataLoader:</div> 表示使用paddle.io.DataLoader从数据集中取数据的阶段</div>'\
-  '<div class="indent"><div class="bold">Forward: </div> 表示模型前向计算的阶段</div>'\
-  '<div class="indent"><div class="bold">Backward: </div> 表示模型反向梯度计算的阶段</div>'\
-  '<div class="indent"><div class="bold">Optimization: </div> 表示模型优化更新参数的阶段</div>'\
-  '<div class="indent"><div class="bold">Other: </div> 其它时间</div>'
+  '<div>展示每一个ProfileStep内模型各阶段DataLoader, Forward, Backward, Optimization以及Other的CPU和GPU时间。<br>'\
+  '<div>CPU时间即是各阶段代码执行的时间，GPU时间是各阶段所调用的GPU Kernel在GPU上的计算时间。<br>'\
+  '&nbsp;&nbsp;<b class="bold">DataLoader: 表示使用paddle.io.DataLoader从数据集中取数据的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Forward: 表示模型前向计算的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Backward: 表示模型反向梯度计算的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Optimization: 表示模型优化更新参数的阶段<br>'\
+  '&nbsp;&nbsp;<b class="bold">Other: 其它时间<br>'
 
 TOOLTIP_EVENT_TYPE_PERSPECTIVE_CN = \
-  '<div>展示不同类型的事件在模型各阶段DataLoader, Forward, Backward, Optimization以及Other的分布。</div>'
+  '展示不同类型的事件在模型各阶段DataLoader, Forward, Backward, Optimization以及Other的分布。'
 
 TOOLTIP_EVENT_TYPE_MODEL_PERSPECTIVE_CN = \
-  '<div>展示在模型各阶段DataLoader, Forward, Backward, Optimization以及Other所包含的各种事件的时间。</div>'
+  '展示在模型各阶段DataLoader, Forward, Backward, Optimization以及Other所包含的各种事件的时间。'
 
 TOOLTIP_DEVICE_INFO_EN = \
-  '<div class="bold">CPU Process Utilization：</div>'\
-  '<div class="indent">Process CPU time / ProfileStep time(total time of profiling）</div>'\
-  '<div class="bold">CPU System Utilization：</div>'\
-  '<div class="indent">Sum of system\'s all processes CPU time/ CPU total time（ProfileStep time* #CPU Core)</div>'\
-  '<div class="bold">GPU Utilization</div>'\
-  '<div class="indent">GPU busy time / ProfileStep time，GPU busy time is the time during in which at \
-    least one GPU kernel is running on it.</div>'\
-  '<div class="bold">Est. SM Efficiency</div>'\
-  '<div class="indent">The SM efficiency for one kernel can be denoted as SM_Eff_i = \
-    min(blocks of this kernel / SM number of this GPU, 100%).'\
-  'Est. SM efficiency of GPU is the weighted sum of SM_Eff_i across all kernels / ProfileStep time</div>'\
-  '<div class="bold">Est. Achieved Occupancy</div>'\
-  '<div class="indent">The SM occupancy for one kernel can be denoted as Occu_i = active warps on an SM / maximum number \
+  '<b class="bold">CPU Process Utilization：</b>'\
+  '&nbsp;&nbsp;Process CPU time / ProfileStep time(total time of profiling）<br>'\
+  '<b class="bold">CPU System Utilization：</b>'\
+  '&nbsp;&nbsp;Sum of system\'s all processes CPU time/ CPU total time（ProfileStep time* #CPU Core)<br>'\
+  '<b class="bold">GPU Utilization</b>'\
+  '&nbsp;&nbsp;GPU busy time / ProfileStep time，GPU busy time is the time during in which at least one GPU kernel is\
+  running on it.<br>'\
+  '<b class="bold">Est. SM Efficiency</b>'\
+  '&nbsp;&nbsp;The SM efficiency for one kernel can be denoted as SM_Eff_i = min(blocks of this kernel / SM number \
+  of this GPU, 100%).'\
+  'Est. SM efficiency of GPU is the weighted sum of SM_Eff_i across all kernels / ProfileStep time<br>'\
+  '<b class="bold">Est. Achieved Occupancy</b>'\
+  '&nbsp;&nbsp;The SM occupancy for one kernel can be denoted as Occu_i = active warps on an SM / maximum number \
     of active warps supported by the SM. \
-  Est. SM occupancy of GPU is the weighted average of Occu_i across all kernels</div>'\
-  '<div class="bold">Tensor cores ratio</div>'\
-  '<div class="indent">Sum of kernel time using Tensor Cores / Sum of total kernel time</div>'
+  Est. SM occupancy of GPU is the weighted average of Occu_i across all kernels<br>'\
+  '<b class="bold">Tensor cores ratio</b>'\
+  '&nbsp;&nbsp;Sum of kernel time using Tensor Cores / Sum of total kernel time<br>'
 
 TOOLTIP_MODEL_PERSPECTIVE_EN = \
-  '<div>Present CPU and GPU time for each stage of a model, i.e. DataLoader, \
-    Forward, Backward, Optimization and Other.</div>'\
-  '<div>CPU time is the execution time for code，GPU time is the calculation time of \
-    kernels launched in the stage.</div>'\
-  '<div class="indent"> <div class="bold">DataLoader:</div>denote data fetching using paddle.io.DataLoader</div>'\
-  '<div class="indent"> <div class="bold">Forward:</div>denote model forward</div>'\
-  '<div class="indent"> <div class="bold">Backward:</div>denote gradient back-propagate</div>'\
-  '<div class="indent"> <div class="bold">Optimization:</div>denote parameters update</div>'\
-  '<div class="indent"> <div class="bold">Other:</div>other time out of above range'
+  'Present CPU and GPU time for each stage of a model, i.e. DataLoader, Forward, Backward, Optimization and Other.<br>'\
+  'CPU time is the execution time for code，GPU time is the calculation time of kernels launched in the stage.<br>'\
+  '&nbsp;&nbsp; <b class="bold">DataLoader:</b> denote data fetching using paddle.io.DataLoader<br>'\
+  '&nbsp;&nbsp; <b class="bold">Forward:</b> denote model forward<br>'\
+  '&nbsp;&nbsp; <b class="bold">Backward:</b> denote gradient back-propagate<br>'\
+  '&nbsp;&nbsp; <b class="bold">Optimization:</b> denote parameters update<br>'\
+  '&nbsp;&nbsp; <b class="bold">Other:</b> other time out of above range'
 
 TOOLTIP_MODEL_PERSPECTIVE_PERSTEP_EN = \
-  '<div>Present CPU and GPU time in each ProfileStep for each stage of a model, \
-  i.e. DataLoader, Forward, Backward, Optimization and Other.</div>'\
-  '<div>CPU time is the execution time for code，GPU time is the calculation time of \
-    kernels launched in the stage.</div>'\
-  '<div class="indent"> <div class="bold">DataLoader:</div>denote data fetching using paddle.io.DataLoader</div>'\
-  '<div class="indent"> <div class="bold">Forward:</div>denote model forward</div>'\
-  '<div class="indent"> <div class="bold">Backward:</div>denote gradient back-propagate</div>'\
-  '<div class="indent"> <div class="bold">Optimization:</div>denote parameters update</div>'\
-  '<div class="indent"> <div class="bold">Other:</div>other time out of above range'
+  'Present CPU and GPU time in each ProfileStep for each stage of a model, \
+  i.e. DataLoader, Forward, Backward, Optimization and Other.<br>'\
+  'CPU time is the execution time for code，GPU time is the calculation time of kernels launched in the stage.<br>'\
+  '&nbsp;&nbsp; <b class="bold">DataLoader:</b> denote data fetching using paddle.io.DataLoader<br>'\
+  '&nbsp;&nbsp; <b class="bold">Forward:</b> denote model forward<br>'\
+  '&nbsp;&nbsp; <b class="bold">Backward:</b> denote gradient back-propagate<br>'\
+  '&nbsp;&nbsp; <b class="bold">Optimization:</b> denote parameters update<br>'\
+  '&nbsp;&nbsp; <b class="bold">Other:</b> other time out of above range'
 
 TOOLTIP_EVENT_TYPE_PERSPECTIVE_EN = \
-  '<div>Present the distribution of each kind of \
-    events across DataLoader, Forward, Backward, Optimization and Other stage.</div>'
+  'Present the distribution of each kind of events across DataLoader,\
+  Forward, Backward, Optimization and Other stage.'
 
 TOOLTIP_EVENT_TYPE_MODEL_PERSPECTIVE_EN = \
-  '<div>Present the time of each kind of events included in DataLoader, \
-    Forward, Backward, Optimization and Other stage.</div>'
+  'Present the time of each kind of events included in DataLoader, Forward, Backward, Optimization and Other stage.'
