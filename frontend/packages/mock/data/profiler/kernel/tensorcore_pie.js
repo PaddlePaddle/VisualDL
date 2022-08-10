74fb9dbdd4c0175a8d227d229a32020d8c2d2b72
xiaoyixin-cmd <1634228212@qq.com>
2022-08-10 10:56:09 +0800

    mockdata remove

--- frontend/packages/mock/data/profiler/kernel/tensorcore_pie.js
+++
@@ -1,7 +0,0 @@
-export default {
-    column_name: ['name', 'calls', 'ratio'],
-    events: [
-        {name: 'Tensor core used', calls: 0.0, ratio: 0.0},
-        {name: 'Tensor core unused', calls: 1362.0, ratio: 1.0}
-    ]
-};
