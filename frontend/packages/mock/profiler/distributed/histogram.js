1f4c9218187b9bd2f3994dd198a838eb41c25eef
xiaoyixin-cmd <1634228212@qq.com>
2022-08-10 15:56:54 +0800

    en

---
+++ frontend/packages/mock/profiler/distributed/histogram.js
@@ -0,0 +1,13 @@
+export default {
+    order: ['Communication', 'Computation', 'Overlap', 'Others'],
+    worker_name: [
+        'host_bjyz-sys-gpu-kongming3.bjyz.baidu.compid_1114',
+        'host_bjyz-sys-gpu-kongming3.bjyz.baidu.compid_1291'
+    ],
+    data: [
+        [0.0, 0.0],
+        [8490.24, 8498.69],
+        [0.0, 0.0],
+        [176892.67, 172642.82]
+    ]
+};
