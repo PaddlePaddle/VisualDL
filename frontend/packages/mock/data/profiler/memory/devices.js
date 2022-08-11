2242ba9f6a48273bb9f83dd23fe7c99c2fe42a03
xiaoyixin-cmd <1634228212@qq.com>
2022-08-11 16:39:31 +0800

    mockdatas

---
+++ frontend/packages/mock/data/profiler/memory/devices.js
@@ -0,0 +1,5 @@
+export default [
+    {device: 'Place(gpu:0)', min_size: 0.0, max_size: 189267.97, max_allocation_size: 9999989},
+    {device: 'Place(gpu_pinned)', min_size: 0.0, max_size: 35487.74, max_allocation_size: 999998},
+    {device: 'Place(cpu)', min_size: 0.0, max_size: 0.01, max_allocation_size: 999998}
+];
