5d6cea52eef38dd4f7c1c177232bd817b73d4803
chenjian <chenjian26@baidu.com>
2022-12-05 14:40:24 +0800

    Add scalars component (#1081) (#1175)
    * backend of add_scalars
    
    * fix function name bug of get_scalars_tags
    
    * @zh794390558 and @ShenYuhan commented 0508
    
    * fix a bug
    
    * adapt to frontend runs need
    
    * fix proto
    
    * fix a bug
    
    * add scalars test demo
    
    * fix
    
    * adapt scalars to scalar
    
    * remove unused code
    
    * fix demo code
    
    * fix proto version
    
    * fix proto version
    
    * upgrade protoc to v3.19.0
    
    * fix a bug in logreader
    
    Co-authored-by: 伽利略 <jialiasus2@163.com>
    Co-authored-by: wuzewu <wuzewu@baidu.com>
    
    Co-authored-by: 伽利略 <jialiasus2@163.com>
    Co-authored-by: wuzewu <wuzewu@baidu.com>

--- visualdl/component/base_component.py
+++ visualdl/component/base_component.py
@@ -18,6 +18,29 @@ from PIL import Image
 from visualdl.proto.record_pb2 import Record
 
 
+def scalars(main_tag, tag_scalar_dict, step, walltime=None):
+    """Package data to scalars
+
+    Args:
+        main_tag (string): Data identifier
+        tag_scalar_dict (dict): A dict to provide multi-values with tags
+        step (int): Step of scalar
+        walltime (int): Wall time of scalar
+
+    Return:
+        Package with format of record_pb2.Record
+    """
+    for sub_tag, value in tag_scalar_dict.items():
+        value = float(value)
+        yield Record(values=[
+            Record.Value(
+                id=step,
+                tag=main_tag,
+                timestamp=walltime,
+                tag_value=Record.TagValue(tag=sub_tag, value=value))
+        ])
+
+
 def scalar(tag, value, step, walltime=None):
     """Package data to one scalar.
     Args:
