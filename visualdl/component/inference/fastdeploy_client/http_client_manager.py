00566df408a86e9226554ba98625d8423a44fed6
chenjian <chenjian26@baidu.com>
2023-01-09 14:27:58 +0800

    fix a bug

--- visualdl/component/inference/fastdeploy_client/http_client_manager.py
+++ visualdl/component/inference/fastdeploy_client/http_client_manager.py
@@ -15,6 +15,7 @@
 import json
 import re
 
+import numpy as np
 import requests
 import tritonclient.http as httpclient
 from attrdict import AttrDict
@@ -40,6 +41,18 @@ def prepare_request(inputs_meta, inputs_data, outputs_meta):
             raise RuntimeError(
                 'Error: input name {} required for model not existed.'.format(
                     input_name))
+        if input_dict['datatype'] == 'FP32':
+            inputs_data[input_name] = inputs_data[input_name].astype(
+                np.float32
+            ) / 255  # image data returned by gradio is uint8, convert to fp32
+        if len(input_dict['shape']
+               ) == 3 and input_dict['shape'][0] == 3:  # NCHW
+            inputs_data[input_name] = inputs_data[input_name][0].transpose(
+                2, 0, 1)
+        elif len(input_dict['shape']
+                 ) == 4 and input_dict['shape'][1] == 3:  # NCHW
+            inputs_data[input_name] = inputs_data[input_name].transpose(
+                0, 3, 1, 2)
         infer_input = httpclient.InferInput(
             input_name, inputs_data[input_name].shape, input_dict['datatype'])
         infer_input.set_data_from_numpy(inputs_data[input_name])
@@ -249,6 +262,7 @@ class HttpClientManager:
                                           output_metadata)
         response = fastdeploy_client.infer(
             model_name, inputs, model_version=model_version, outputs=outputs)
+
         results = {}
         for output in output_metadata:
             result = response.as_numpy(output.name)  # datatype: numpy
